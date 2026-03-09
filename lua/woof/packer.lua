c0be3548a742c29ca938cfe5f1a356a707697d7b
acutewoof <sutharshanvithushan@gmail.com>
2026-03-09 06:23:52 +0530

    base16

--- lua/woof/packer.lua
+++ lua/woof/packer.lua
@@ -1,26 +1,18 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
   use {
 	  'nvim-telescope/telescope.nvim',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-
   use (
   {
 	  "folke/tokyonight.nvim",
 	  as = "tokyonight",
-	  config = function()
-		  vim.cmd('colorscheme tokyonight-night')
-	  end
   }
   )
-
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
   use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
   use {
 	  "ThePrimeagen/harpoon",
@@ -35,11 +27,14 @@ return require('packer').startup(function(use)
   requires = {
     {'williamboman/mason.nvim'},
     {'williamboman/mason-lspconfig.nvim'},
-
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/nvim-cmp'},
     {'hrsh7th/cmp-nvim-lsp'},
     {'L3MON4D3/LuaSnip'},
   }}
   use "neovim/nvim-lspconfig"
+  use ({"rrethy/base16-nvim"
+	  config = function()
+		  vim.cmd('colorscheme base16-gruvbox-material-dark-hard')
+	  end})
 end)
